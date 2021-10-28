4957a074aec807e22ec7a52f68b4c1128331268b
spata00 <sararares12@gmail.com>
2021-10-28 19:03:43 +0200

    finito

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,12 @@ class HomeController extends Controller
 {
     public function Home(){
         $movie = Movie::all();
-        return view('home',compact('$movie'));
+        return view('home',compact('movie'));
+    }
+
+    public function Details($id){
+        $movie = Movie::where('id', $id)->get();
+        
+        return view('home',compact('movie'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,9 @@
 @extends('layouts.app')
 
-
+@section('content')
+    @foreach($movie as $film)
+        <a href="{{ route('pagina-dettaglio',['id'=>$film['id']] )}}">
+            <p>{{$film['title']}}</p>
+        </a>
+    @endforeach
+@endsection
